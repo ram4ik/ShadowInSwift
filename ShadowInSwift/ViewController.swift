4ef854256527df32877b912c03894c5fe24a2b5b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-27 19:33:24 +0200

    init

--- ShadowInSwift/ViewController.swift
+++ ShadowInSwift/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let shadowView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        shadowView.backgroundColor = .systemGray
+        shadowView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(shadowView)
+        
+        NSLayoutConstraint.activate([
+            shadowView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shadowView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            shadowView.widthAnchor.constraint(equalToConstant: 300),
+            shadowView.heightAnchor.constraint(equalToConstant: 200),
+        ])
+        
+        shadowView.layer.shadowOpacity = 0.5
+        shadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
     }
 
 
